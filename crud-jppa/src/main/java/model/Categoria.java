fa64e6e949172107aa2cee8d2b811a7452c1818b
Letícia Zalasik <leticiazalasik@gmail.com>
2024-07-02 20:48:40 -0300

    :construction: Feat (model, repository): Criaçao da classe e da inetrface categoria.

---
+++ crud-jppa/src/main/java/model/Categoria.java
@@ -0,0 +1,35 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+//Define automaticamente o construturo vazio da classe
+@NoArgsConstructor
+
+//Define automaticamente o construtor com TODOS os atributos da classe
+@AllArgsConstructor
+
+//Define os getters e setters automaticamente
+//Ou pode substituir pela anotação @Data
+@Getter @Setter
+
+//Gera todas as possibilidades de construtores que nao sao os vazios ou os totalmente cheios
+@Builder
+
+//Gera automaticamente uma tabela no banco de dados, tendo as colunas dessa tabela de forma
+// espelhada com os atributos da classe categoria
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    
+    private String nome;
+    private boolean status;
+
+}
