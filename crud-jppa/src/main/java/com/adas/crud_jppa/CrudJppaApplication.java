011eb457089f02423eae46ea0af11cee1c262144
Letícia Zalasik <leticiazalasik@gmail.com>
2024-07-02 18:49:52 -0300

    :construction: Feat (crud-jpa): Criando o aqruivo spring.

---
+++ crud-jppa/src/main/java/com/adas/crud_jppa/CrudJppaApplication.java
@@ -0,0 +1,13 @@
+package com.adas.crud_jppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJppaApplication.class, args);
+	}
+
+}
---
+++ crud-jppa/src/test/java/com/adas/crud_jppa/CrudJppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adas.crud_jppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
