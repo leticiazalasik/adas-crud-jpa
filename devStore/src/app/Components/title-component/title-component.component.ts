7025073b3ebf49204403680edc48c25428b950e4
Letícia Zalasik <leticiazalasik@gmail.com>
2024-07-31 16:08:26 -0300

    :construction: Feat (header,title-component): Criação do HTML e CSS desses dois componentes.

---
+++ devStore/src/app/Components/title-component/title-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-component',
+  templateUrl: './title-component.component.html',
+  styleUrl: './title-component.component.css'
+})
+export class TitleComponentComponent {
+
+}
--- devStore/src/app/app.module.ts
+++ devStore/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { TitleComponentComponent } from './Components/title-component/title-component.component';
+import { MenuProdutoComponentComponent } from './Components/menu-produto-component/menu-produto-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponentComponent,
+    MenuProdutoComponentComponent
   ],
   imports: [
     BrowserModule,
