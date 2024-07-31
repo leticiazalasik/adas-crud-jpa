4ef93c8cb644be5a25c581ad2e2aa35d964724c1
Letícia Zalasik <leticiazalasik@gmail.com>
2024-07-31 10:48:49 -0300

    :construction: Feat (CategoriaRepository, CategoriaService, CategoriaController): implementação do método e endpoint updateCategoriaById usando @Modifying e @transactional.

--- crud-jppa/src/main/java/com/adas/crud_jppa/ExemploMap.java
+++ crud-jppa/src/main/java/com/adas/crud_jppa/ExemploMap.java
@@ -20,34 +20,6 @@ public class ExemploMap {
         System.out.println("Nome: " + objetoGenereico.get("NOME"));
         System.out.println("Status: " + objetoGenereico.get("STATUS"));
 
-        import java.util.HashMap;
-import java.util.Map;
-
-        public class ConsultaComHashMap {
-
-            // Simulando um banco de dados (apenas para exemplo)
-            private static final Map<Integer, String> bancoDeDados = new HashMap<>();
-
-            // Método para executar a consulta
-            public static String executarConsulta(HashMap<String, Object> criterios) {
-                // Aqui, você pode construir sua consulta real usando os critérios do HashMap
-                // Por exemplo, se você quiser buscar um produto pelo ID:
-                Integer idProduto = (Integer) criterios.get("id");
-                return bancoDeDados.get(idProduto);
-            }
-
-            public static void main(String[] args) {
-                // Exemplo de uso
-                bancoDeDados.put(1, "Produto A");
-                bancoDeDados.put(2, "Produto B");
-
-                HashMap<String, Object> criterios = new HashMap<>();
-                criterios.put("id", 1); // Buscar produto com ID 1
-
-                String resultado = executarConsulta(criterios);
-                System.out.println("Resultado da consulta: " + resultado);
-            }
-        }
 
 
     }
